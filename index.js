da188041fd355b62f8f39a8e698a89f14a2028fc
jfrey13 <jfrey@hcssupport.com>
2019-07-25 13:06:31 -0400

    Fix broken condition

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ module.exports = (allResults) => {
   allResults.forEach((result) => {
     const { warnings } = result;
 
-    if (warnings.length) {
+    if (!warnings.length) {
       return;
     }
 
